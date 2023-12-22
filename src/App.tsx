023d94d812ce0e2037036568f447ea88c4ca31b2
claramarsango <claraxmarsango@gmail.com>
2023-12-22 12:53:44 +0100

    add user feedback message

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css';
 
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <header className="header">
+        <h1>Chess</h1>
+      </header>
+      <main className="game-container">
+        <section className="game__progress-message">
+          <h3>🚧 ...Under construction... 🚧</h3>
+          <p>Thanks for stopping by! 🦦</p>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default App;
