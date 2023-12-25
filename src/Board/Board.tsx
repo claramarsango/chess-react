75864bdd8fe17e0f39e3d8030602335c6e3033af
claramarsango <claraxmarsango@gmail.com>
2023-12-25 20:26:42 +0100

    add board borders with rows and columns indexes

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Square from '../Square/Square';
 import './board-styled.css';
+import BoardBorder from '../BoardBorder/BoardBorder';
 
 const COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 const ROWS = Array.from({ length: 8 }, (_, index) => index + 1).reverse();
@@ -26,11 +27,19 @@ const Board = () => {
 
   return (
     <section className="board-container">
-      <section className="board__squares-container">
-        {board.map(square => (
-          <Square key={square.join('')} position={square} />
-        ))}
-      </section>
+      <BoardBorder>{ROWS}</BoardBorder>
+
+      <div className="board__central-section">
+        <BoardBorder>{COLUMNS}</BoardBorder>
+        <section className="board__squares-container">
+          {board.map(square => (
+            <Square key={square.join('')} position={square} />
+          ))}
+        </section>
+        <BoardBorder>{COLUMNS}</BoardBorder>
+      </div>
+
+      <BoardBorder>{ROWS}</BoardBorder>
     </section>
   );
 };
