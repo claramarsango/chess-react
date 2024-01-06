00a554caa99502f81dac474aea113e716b55ce71
claramarsango <claraxmarsango@gmail.com>
2024-01-06 15:41:20 +0100

    specify preset for jest config

--- jest.config.ts
+++ jest.config.ts
@@ -103,7 +103,7 @@ const config: Config = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  /* preset: 'ts-jest', */
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
