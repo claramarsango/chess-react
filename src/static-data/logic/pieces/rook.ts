c1919515ad3f4f2c2d93680a3dbb2bcb1d4b858a
claramarsango <claraxmarsango@gmail.com>
2024-03-22 22:39:10 +0100

    refactor: extract opponent pieces search logic from rook captures logic

--- src/static-data/logic/pieces/rook.ts
+++ src/static-data/logic/pieces/rook.ts
@@ -18,22 +18,38 @@ export const possibleRookMoves = (
   return allPossibleMoves;
 };
 
+const findOpponentPiecesFrom = (
+  capturablePositions: string[],
+  piecesOnBoard: PieceModel[],
+  currentPiece: PieceModel,
+) => {
+  const filteredOpponentPieces: PieceModel[] = [];
+
+  for (const position of capturablePositions) {
+    const opponentPiece = piecesOnBoard.find(
+      piece =>
+        piece.position === position && piece.player !== currentPiece.player,
+    );
+    if (opponentPiece) filteredOpponentPieces.push(opponentPiece);
+  }
+
+  return filteredOpponentPieces;
+};
+
 export const possibleRookCaptures = (
   rook: PieceModel,
   piecesOnBoard: PieceModel[],
 ) => {
-  const possibleCaptures = [];
   const closestPiecePositions = findClosestRowPiecesFrom(
     rook,
     piecesOnBoard,
   ).concat(findClosestColumnPiecesFrom(rook, piecesOnBoard));
 
-  for (const position of closestPiecePositions) {
-    const opponentPiece = piecesOnBoard.find(
-      piece => piece.position === position && piece.player !== rook.player,
-    );
-    if (opponentPiece) possibleCaptures.push(opponentPiece);
-  }
+  const possibleCaptures = findOpponentPiecesFrom(
+    closestPiecePositions,
+    piecesOnBoard,
+    rook,
+  );
 
   return possibleCaptures;
 };
